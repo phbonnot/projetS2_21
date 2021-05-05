7cb8f0cafd56281cb604cc0fa032b4ae69b09d5e
phbonnot <phbonnot@gmail.com>
2021-05-05 18:24:32 +0200

    réglage des dimensions

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../vue/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("World of Zelda");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
